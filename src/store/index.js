5453642c3bbaf464d51e472af032f763edeabe93
Nazar Koval <n.kovaldev@gmail.com>
2023-03-22 05:20:42 +0200

    feat: init redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,22 @@
 import { createStore } from "redux"
-import { saveFilters, loadFilters } from "./local-storage"
+// import { saveFilters, loadFilters } from "./local-storage"
 import rootReducer from "./root-reducer"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
-const store = createStore(rootReducer, loadFilters(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const persistConfig = {
+	key: "root",
+	storage,
+   whitelist: ["filters"],
+   // blacklist: []
+}
 
-store.subscribe(() => saveFilters({ filters: store.getState().filters }))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+// const store = createStore(rootReducer, loadFilters(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// store.subscribe(() => saveFilters({ filters: store.getState().filters }))
 
 export default store
+export const persistor = persistStore(store)

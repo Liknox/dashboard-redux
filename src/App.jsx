64a1ef08096b40aff6a6c6c19792c551e8af637f
Nazar Koval <n.kovaldev@gmail.com>
2023-03-22 06:11:09 +0200

    feat: api thunk operation

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { useDispatch } from "react-redux"
 import { FilterPanel } from "./components/FilterPanel"
 import { JobList } from "./components/JobList"
 import { TheHeader } from "./components/TheHeader"
-import { addPositions } from "./store/positions/position-actions"
-
-import data from "./mock/data.json"
+import { loadPositions } from "./store/positions/position-actions"
 
 function App() {
 	const dispatch = useDispatch()
 
 	useEffect(() => {
-		dispatch(addPositions(data))
+		dispatch(loadPositions())
 	})
 
 	return (
--- src/store/positions/position-actions.js
+++ src/store/positions/position-actions.js
@@ -1,6 +1,14 @@
+import { API_URL } from "../config"
+
 export const ADD_POSITIONS = "ADD_POSITIONS"
 
-export const addPositions = positions => ({
+ const addPositions = positions => ({
 	type: ADD_POSITIONS,
 	positions
 })
+
+export const loadPositions = () => dispatch => {
+	fetch(API_URL)
+		.then(res => res.json())
+		.then(data => dispatch(addPositions(data)))
+}
